4ae171ecefc92fdcb020c91a8811ec02d8bd18a1
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-03-11 00:46:49 +0000

    added the register method with the testing  with postman

--- CarRentalAPI/app/Http/Controllers/authController.php
+++ CarRentalAPI/app/Http/Controllers/authController.php
@@ -2,47 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class authController extends Controller
+class AuthController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+    public function register(Request $request){
+        $request->validate([
+            'name' =>'required|string|min:3',
+            'email'=>'required|unique:users,email',
+            'password'=>'required|confirmed',
+        ]);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $user = User::create([
+                'name'=>$request->name,
+                'email'=>$request->email,
+                'password'=>Hash::make($request->password),
+                'api_token' => Str::random(60),
+        ]);
+        return response()->json(['token'=>$user->api_token],201);
     }
 }
--- CarRentalAPI/routes/api.php
+++ CarRentalAPI/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\RentalController;
 use Illuminate\Http\Request;
@@ -8,3 +9,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('cars', CarController::class);
 Route::apiResource('rentals', RentalController::class);
+Route::post('register', [AuthController::class, 'register']);
